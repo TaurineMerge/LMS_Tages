547bde6303cb532ce6637c505d52a554cde21f57
Плотников Владислав <plotnikov.vladislav@MacBook-Pro-Tages-5.local>
2025-12-09 19:07:52 +0300

    chore: create env variables in main

--- testing/src/main/java/com/example/lms/Main.java
+++ testing/src/main/java/com/example/lms/Main.java
@@ -1,5 +1,7 @@
 package com.example.lms;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import io.javalin.Javalin;
 import com.example.lms.test.api.router.TestRouter;
 import com.example.lms.test.domain.service.TestService;
@@ -10,8 +12,17 @@ import com.example.lms.test.api.controller.TestController;
 public class Main {
 
     public static void main(String[] args) {
+        Dotenv dotenv = null;
+        
+        dotenv = Dotenv.load();
+        
+        final Integer APP_PORT = Integer.parseInt(dotenv.get("APP_PORT"));
+        final String DB_URL = dotenv.get("DB_URL");
+        final String DB_USER = dotenv.get("DB_USER");
+        final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+        
         // Manual Dependency Injection
-        DatabaseConfig dbConfig = new DatabaseConfig("jdbc:postgresql://app-db/appdb", "appuser", "password");
+        DatabaseConfig dbConfig = new DatabaseConfig(DB_URL, DB_USER, DB_PASSWORD);
         TestRepository testRepository = new TestRepository(dbConfig);
         TestService testService = new TestService(testRepository);
         TestController testController = new TestController(testService);
@@ -21,6 +32,6 @@ public class Main {
             config.router.apiBuilder(() -> {
                 TestRouter.register(testController);
             });
-        }).start(8085);
+        }).start(APP_PORT);
     }
 }
